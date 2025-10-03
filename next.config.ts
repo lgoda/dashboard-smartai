71b80c88914b3bd490f6b1ab5297f675e3e0ee64
lgoda <luca.godano@gmail.com>
2025-10-03 16:09:43 +0200

    Updated next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,10 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
+  env: {
+    NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
+    NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+  },
 };
 
 module.exports = nextConfig;
